3d4abe950db37ee7eebb33f242bb45b90256c353
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-03 15:01:40 +0200

    initial commit

---
+++ src/test/java/dev/team1/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
