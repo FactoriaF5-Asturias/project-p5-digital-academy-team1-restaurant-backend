c9e8ec8f6d2c61c3466454a2c908f65666f47259
Jose Luis <jlcgmieres15@gmail.com>
2026-09-22 12:07:16 +0200

    create the InvoiceDTOResponse record

--- src/main/java/dev/team1/invoices/dtos/InvoiceDTOResponse.java
+++ src/main/java/dev/team1/invoices/dtos/InvoiceDTOResponse.java
@@ -1,5 +1,18 @@
 package dev.team1.invoices.dtos;
 
-public record InvoiceDTOResponse() {
+import java.math.BigDecimal;
+import java.time.Instant;
 
+import dev.team1.orders.OrderEntity;
+import lombok.Builder;
+
+@Builder 
+public record InvoiceDTOResponse(
+  Long id,
+  OrderEntity order,
+  String invoice_number,
+  BigDecimal amount,
+  Instant paid_at
+) {
+  
 }
