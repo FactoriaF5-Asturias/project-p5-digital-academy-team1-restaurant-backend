d85b4fb940f7f2fd0ee82e9fd1401ba50d8765d5
Jose Luis <jlcgmieres15@gmail.com>
2026-09-23 14:54:17 +0200

    Making the InvoiceDTORequest

--- src/main/java/dev/team1/invoices/dtos/InvoiceDTORequest.java
+++ src/main/java/dev/team1/invoices/dtos/InvoiceDTORequest.java
@@ -1,12 +1,27 @@
 package dev.team1.invoices.dtos;
 
+import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.UUID;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 public record InvoiceDTORequest(
   @NotBlank(message = "El campo 'invoice_number no debe de ser vacío")
-  UUID invoiceNumber
+  UUID invoiceNumber,
+
+  @NotNull(message = "El campo 'amount' es obligatorio")
+  @DecimalMin (value = "0.0", inclusive = false)
+  @Digits(integer = 10, fraction = 2, message = "El campo 'amount' debe estar en formato 0.00 con 10 dígitos máximo en un número y dos dígitos después de punto decimal")
+  BigDecimal amount,
+
+  @NotNull(message = "El campo 'paidAt' es obligatorio")
+  @PastOrPresent(message = "La fecha de pago no puede ser futura")
+  Instant paidAt
 ) {
 
 }
