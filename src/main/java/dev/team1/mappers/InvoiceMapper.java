55635716a1cc9739d622f0be7450ff01a2403e3e
Jose Luis <jlcgmieres15@gmail.com>
2026-09-24 12:19:15 +0200

    InvoiceMapper InvoiceDTOResponse-->toDTO()

--- src/main/java/dev/team1/mappers/InvoiceMapper.java
+++ src/main/java/dev/team1/mappers/InvoiceMapper.java
@@ -1,5 +1,21 @@
 package dev.team1.mappers;
 
+import dev.team1.invoices.InvoiceEntity;
+import dev.team1.invoices.dtos.InvoiceDTOResponse;
+
 public class InvoiceMapper {
 
+  private InvoiceMapper() {
+  }
+
+  public static InvoiceDTOResponse toDTO(InvoiceEntity entity) {
+    return InvoiceDTOResponse.builder()
+      .id(entity.getId())
+      .orderId(entity.getOrder() != null ? entity.getOrder().getId() : null)
+      .invoiceNumber(entity.getInvoiceNumber())
+      .amount(entity.getAmount())
+      .paidAt(entity.getPaidAt())
+      .build()
+    ;
+  }
 }
