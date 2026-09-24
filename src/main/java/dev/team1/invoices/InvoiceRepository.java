ce0152eb30d5fe7dcf02b651f8606437915c56a6
Jose Luis <jlcgmieres15@gmail.com>
2026-09-24 10:41:18 +0200

    InvoiceRepository finalized

--- src/main/java/dev/team1/invoices/InvoiceRepository.java
+++ src/main/java/dev/team1/invoices/InvoiceRepository.java
@@ -1,8 +1,13 @@
 package dev.team1.invoices;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InvoiceRepository // extends JpaRepository<InvoiceEntity, Long>
-{
+public interface InvoiceRepository extends JpaRepository<InvoiceEntity, Long> {
 
+  public Optional<InvoiceEntity> findByInvoice_number(UUID invoiceNumber);
+
+  
 }
