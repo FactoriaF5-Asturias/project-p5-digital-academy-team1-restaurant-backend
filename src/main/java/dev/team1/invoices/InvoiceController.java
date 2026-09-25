66625ca477b5582e844a7aa1e031a397c4fa5dc1
Jose Luis <jlcgmieres15@gmail.com>
2026-09-25 15:16:27 +0200

    Invoice controller

--- src/main/java/dev/team1/invoices/InvoiceController.java
+++ src/main/java/dev/team1/invoices/InvoiceController.java
@@ -1,5 +1,38 @@
 package dev.team1.invoices;
 
-public class InvoiceController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import dev.team1.contracts.IInvoiceService;
+import dev.team1.invoices.dtos.InvoiceDTORequest;
+import dev.team1.invoices.dtos.InvoiceDTOResponse;
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+@RequestMapping(path = "${api-endpoint}/invoices")
+public class InvoiceController {
+  private final IInvoiceService invoiceService;
+
+  public InvoiceController(IInvoiceService invoiceService) {
+    this.invoiceService = invoiceService;
+  }
+
+  @PostMapping("")
+  public ResponseEntity<InvoiceDTOResponse> create(@Valid @RequestBody InvoiceDTORequest dto) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(invoiceService.create(dto));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<InvoiceDTOResponse> findById(@PathVariable Long id) {
+      return ResponseEntity.ok(invoiceService.findById(id));
+  }
 }
